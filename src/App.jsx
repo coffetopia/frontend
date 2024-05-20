c2085655b831ac17cfdfbd47f0a5f6a6b2008954
AnnisaMaghfirah <2110817220002@mhs.ulm.ac.id>
2024-05-20 16:59:58 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    AOS.init(
+      {
+        offset: 100,
+        duration: 700,
+        easing: "ease-in",
+        delay: 100,
+      },
+      []
+    );
+  });
   return (
     <Router>
       <Routes>
