d97b8b252d8340746e12c2d1af10f985eea740c0
nabilla <nabillarifda849@gmail.com>
2024-06-20 12:59:26 +0700

    update checkbox payment

--- src/components/buttoncheckout/Buttonbank.jsx
+++ src/components/buttoncheckout/Buttonbank.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/react';
-import bank from "../../assets/logobutton/bank.png";
-// Path menuju gambar logo
+import bank from "../../assets/logobutton/bank.png"; // Path menuju gambar logo
 
 const cardStyle = css`
     display: flex;
@@ -22,6 +21,7 @@ const cardStyle = css`
         border: none;
         border-radius: 5px;
         cursor: pointer;
+        position: relative;
 
         .icon {
             width: 10px; /* Ukuran gambar ikon diubah menjadi 10px */
@@ -33,26 +33,62 @@ const cardStyle = css`
             font-weight: bold;
         }
     }
+
+    input[type="checkbox"] {
+        display: none;
+    }
+
+   /* CSS untuk custom checkbox */
+.custom-checkbox {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border: 2px solid #ccc;
+    border-radius: 3px;
+    background-color: white;
+    cursor: pointer;
+    position: relative;
+    outline: none;
+}
+
+.custom-checkbox:checked {
+    border-color: #F4991A; /* Warna oranye */
+    background-color: #F4991A; /* Warna oranye */
+}
+
+.custom-checkbox:checked:after {
+    content: '';
+    position: absolute;
+    top: 2px;
+    left: 7px;
+    width: 4px;
+    height: 8px;
+    border: solid white;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+}
+
 `;
 
 const Buttonbank = () => {
     return (
-        <div css={cardStyle} className="flex items-center">
-        <button className="btn flex items-center relative">
-        <input type="checkbox" className="hidden" />
-            <div className="border-2 border-[#321313] rounded-full bg-white w-4 h-4 absolute left-[20px]"></div>
-            <img
-                src={bank}
-                alt="card"
-                className="icon bg-[#F4991A] border-[#321313] border rounded-lg"
-                style={{ width: '50px', height: '50px', marginRight: '36px', marginLeft: '60px' }}
+        <div css={cardStyle} className="flex items-center">   
+            <label className="inline-flex items-center ml-5 ">
+            <input
+                type="checkbox"
+                className="custom-checkbox "    
             />
-            <span className="font-bold text-[#321313] ml-2">Bank account</span>
-        </button>
-    </div>
-    
-
-
+            </label>
+                <img
+                    src={bank}
+                    alt="card"
+                    className="icon bg-[#F4991A] border-[#321313] border rounded-lg"
+                    style={{ width: '50px', height: '50px', marginRight: '36px', marginLeft: '20px' }}
+                />
+                <span className="font-bold text-[#321313] ml-2">Bank account</span> 
+        </div>
     );
 };
 
